66b362e171b6d5536f380bbc09367286bb486881
Gergo Pula <gergopula@outlook.com>
2020-07-07 18:17:19 -0400

    Finished HW

--- hw.py
+++ hw.py
@@ -9,3 +9,50 @@ Req:
     2. it should work as expected
     3. do the time complexity analysis
 """
+
+def replace_nums(a):
+
+    replaced_nums=[]
+
+    if(len(a)!=0):
+
+        for i in range(len(a)-1):
+            r=a[(i+1):]
+
+            replaced_nums.append(max(r))
+
+        replaced_nums.append(-1)
+
+        return replaced_nums
+    
+    else:    
+        return a
+
+
+print(replace_nums([1,5,63,-1,-4,-2]))
+print(replace_nums([4,12,124,623,-214,-21,-42,-523]))
+print(replace_nums([]))
+print(replace_nums([1]))
+
+def replace_nums2(num_list):
+    new_list=[-1]
+    if len(num_list)!=0:
+        list_max=min(num_list)
+        for i in range(len(num_list)-1,-1,-1):
+        
+            if num_list[i]>list_max:
+                list_max=num_list[i]
+            else:
+                num_list[i]=list_max
+        
+            new_list.insert(0,num_list[i])
+        new_list.pop(0)
+        return(new_list)
+    
+    else:
+        return([])
+
+print(replace_nums2([4,12,124,623,-214,-21,-42,-523]))
+print(replace_nums2([]))
+print(replace_nums2([1]))
+print(replace_nums2([1,5,63,-1,-4,-2]))
